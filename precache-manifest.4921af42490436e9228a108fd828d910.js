a3c47463a2cefd650cf9ddaf1a1be17b55e4d25f
Kimleang Nao <kimleangn@gmail.com>
2020-07-13 10:40:24 -0700

    Updates

---
+++ precache-manifest.4921af42490436e9228a108fd828d910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f117fdc4320fc6dc826e24bbbdcdc407",
+    "url": "/hamster/index.html"
+  },
+  {
+    "revision": "3e407c1fe5f1ffb57755",
+    "url": "/hamster/static/css/main.c698d9dd.chunk.css"
+  },
+  {
+    "revision": "cd848d61474c4801a326",
+    "url": "/hamster/static/js/2.adfaa3c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hamster/static/js/2.adfaa3c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e407c1fe5f1ffb57755",
+    "url": "/hamster/static/js/main.3d2812d6.chunk.js"
+  },
+  {
+    "revision": "49fc0c3a96aa640200f8",
+    "url": "/hamster/static/js/runtime-main.d782a3f9.js"
+  },
+  {
+    "revision": "aaa3241f65a2537eb6a5f4240805e5db",
+    "url": "/hamster/static/media/hamster.aaa3241f.JPG"
+  }
+]);
